d9dc52b498148c8edb4bbfb2099a6068988a4590
Ariel-Wong <arielwong@ArieldeMacBook-Pro.local>
2020-03-15 21:04:19 +0800

    第二周作业

---
+++ ArrayCalculate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Array_Calculate
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            Console.WriteLine("输入"+num.Length+"个整数：");
+            int count=0, max, min,sum;
+            for  (; count < num.Length;count++)
+                num[count] = int.Parse(Console.ReadLine());
+            for(count=1,max=min=sum=num[0];count<num.Length;count++)
+            {
+                sum += num[count];
+                if (num[count] > max) max = num[count];
+                else if (num[count] < min) min = num[count];
+            }
+            Console.WriteLine("最大数为：" + max);
+            Console.WriteLine("最小数为：" + min);
+            Console.WriteLine("总和为：" + sum);
+            Console.WriteLine("平均数为：" + (sum / num.Length));
+        }
+    }
+}
---
+++ SievOfEratosthenes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SievOfEratosthenes
+{
+    class MainClass
+    {
+        const int maxNum = 101;
+        const int squareRoot = 11;
+        public static void Main(string[] args)
+        {
+            int[] num = new int[maxNum];
+            int i,j;
+            //]将非素数置1
+            for(i=2;i<squareRoot;i++)
+            {
+                if (num[i] == 1) continue;
+                for(j=i+1;j<maxNum;j++)
+                {
+                    if (j % i == 0) num[j] = 1;
+                }
+            }
+            //输出所有素数
+            Console.WriteLine("2～100以内的素数有：");
+            for(i=2;i<maxNum;i++)
+            {
+                if (num[i] == 0) Console.WriteLine(i);  
+            }
+        }
+    }
+}
