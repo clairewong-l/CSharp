421cbe0b098e48f95416a20c2fdf186a619d3e5b
Ariel-Wong <arielwong@ArieldeMacBook-Pro.local>
2020-03-15 17:56:17 +0800

    test

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Shape_Calculate
+{
+    class shape
+    {
+
+    }
+    class triangle : shape
+    {
+
+    }
+    class square : shape
+    {
+
+    }
+    class tectangle : square
+    {
+
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
