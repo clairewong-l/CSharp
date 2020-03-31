6619ede5500fa183fded8835fe6b2f157b80d94c
Ariel-Wong <1395455592@qq.com>
2020-03-31 22:11:45 +0800

    第四周作业

---
+++ Clock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace MyClock
+{
+    public class Clock
+    {
+        //声明事件
+        public event TimerCallback TimerCB;
+        public void Do(string alarmTime)
+        {
+            Timer t = new Timer(TimerCB, alarmTime, 0, 1000);   //一秒钟调用一次,实现计时功能
+            Console.WriteLine("Press Enter to stop the clock!");
+            Console.ReadLine();
+        }
+    }
+    public class ClockEvent
+    {
+        public Clock clock ;
+        public ClockEvent(Clock c)
+        {
+            clock = c;
+            clock.TimerCB += Tick;  //事件的订阅
+            clock.TimerCB += Alarm; //事件的订阅
+        }
+        //为clock事件添加两个处理方法
+        static void Tick(object state)
+        {
+            Console.WriteLine("Tick...Time is: {0}", DateTime.Now.ToLongTimeString());
+        }
+        static void Alarm(object state)
+        {
+            if (DateTime.Now.ToLongTimeString().Equals(state))
+            {
+                Console.WriteLine("Alarm! It' s {0}", DateTime.Now.ToLongTimeString());
+            }
+        }
+    }
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set the alarm by inputting xx:xx:xx!");
+            string alarmTime = Console.ReadLine();
+            Clock clock = new Clock();
+            ClockEvent b = new ClockEvent(clock);
+            clock.Do(alarmTime);
+        }
+    }
+
+}
---
+++ ForEach.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ForEach
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> genericList = new GenericList<int>();
+            int sum = 0,max,min;
+            Random rd = new Random();
+       
+            for(int i=0;i<10;i++)
+            {
+                genericList.Add(rd.Next(1,100));
+            }
+            max = genericList.Head.Data;
+            min = genericList.Head.Data;
+            Action<int> show = m => { Console.WriteLine(m); };
+            Action<int> getMax = m => { if (m > max) max = m; };
+            Action<int> getMin = m => { if (m < min) min = m; };
+            Action<int> getSum = m => sum += m; ;
+            genericList.ForEach(show);
+            genericList.ForEach(getMax);
+            genericList.ForEach(getMin);
+            genericList.ForEach(getSum);
+            Console.WriteLine($"max={max}");
+            Console.WriteLine($"min={min}");
+            Console.WriteLine($"sum={sum}");
+        }
+    }
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+       
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> p = this.head;
+            for (; p != null; p = p.Next)
+            {
+                action(p.Data);
+            }
+        }
+    }
+}
